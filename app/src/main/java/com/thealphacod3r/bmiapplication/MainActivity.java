0a9300c024a36809437a16aaea512160bfe68873
thealphacod3r <thealphacod3r@gmail.com>
2024-08-23 21:08:06 +0530

    Designing Done!

--- app/src/main/java/com/thealphacod3r/bmiapplication/MainActivity.java
+++ app/src/main/java/com/thealphacod3r/bmiapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.thealphacod3r.bmiapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        TextView txt;
+        EditText editWeight, editHeight, editAge;
+        Button btnCalculate;
+
+        editWeight = findViewById(R.id.editWeight);
+        editHeight = findViewById(R.id.editHeight);
+        editAge = findViewById(R.id.editAge);
+        btnCalculate = findViewById(R.id.btnCalculate);
+        txt = findViewById(R.id.txtResult);
     }
 }
